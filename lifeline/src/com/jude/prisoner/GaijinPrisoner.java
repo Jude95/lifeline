d424a47d938acc18cd705a8cf3b6601e548259c2
ㄟ( ▔, ▔ )ㄏ <tlgithub@sina.com>
2015-11-02 16:51:10 +0800

    改进了一下
    改进了一下 用了平均数

--- lifeline/src/com/jude/prisoner/GaijinPrisoner.java
+++ lifeline/src/com/jude/prisoner/GaijinPrisoner.java
@@ -1,18 +1,18 @@
 package com.jude.prisoner;
 
 import com.jude.Prisoner;
-import com.jude.manager.Manager;
+import com.jude.Manager;
 
 /**
  * Created by Mr.Jude on 2015/10/28.
  */
-public class FuckGodPrisoner implements Prisoner {
+public class GaijinPrisoner implements Prisoner {
     int totalCount;
     int totalPerson;
 
     @Override
     public String getName() {
-        return "唐磊2015213872";
+        return "鍞愮2015213872";
     }
 
     @Override
@@ -23,20 +23,20 @@ public class FuckGodPrisoner implements Prisoner {
 
     @Override
     public int take(int index, int last) {
-    	int average = totalCount/totalPerson;
-    	int lastaverage = last/(totalPerson - (index+1));  //我觉得可能应该可以用到平均数
+    	int average = totalCount / totalPerson;
+    	int lastaverage = (totalCount - last)/index;
     	if(index == 0 ){
-    		return totalCount - totalPerson; //如果我在第一个，不要怪我了
+    		return average;
     	}
     	else{
-    		if(lastaverage > average){
-    			return average;
+    		if(lastaverage<average){
+    			return lastaverage-1;
     		}
-    		if(lastaverage < average){
-    			return average+1;
+    		if(lastaverage>average){
+    			return lastaverage+1;
     		}
     		else{
-    			return average-1;
+    			return lastaverage;
     		}
     	}
     }
@@ -47,3 +47,5 @@ public class FuckGodPrisoner implements Prisoner {
     }
 
 }
+
+
