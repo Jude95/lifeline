73b2e93bbc0fec6d46434422a74bd684062f9d4d
X-Frank-D <975688206@qq.com>
2015-11-03 15:14:49 +0800

    test

--- lifeline/src/com/jude/prisoner/ZhangDeShengPrisoner.java
+++ lifeline/src/com/jude/prisoner/ZhangDeShengPrisoner.java
@@ -7,7 +7,7 @@ import com.sun.org.apache.xpath.internal.SourceTree;
  */
 public class ZhangDeShengPrisoner {
     public void main(String args[]){
-        System.out.print("≤‚ ‘“ªœ¬github");
+        System.out.print("≤‚ ‘github");
     }
 
 }
