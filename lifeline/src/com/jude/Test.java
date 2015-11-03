691e3028923a3978a046c4cb708f39ee4b28fed9
Administrator <18983679028@163.com>
2015-11-03 15:01:56 +0800

    test

--- lifeline/src/com/jude/Test.java
+++ lifeline/src/com/jude/Test.java
@@ -7,5 +7,8 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("what is pull quest?");
+
+        System.out.println("sdldkflsddlfkskld");
+        System.out.println("-------");
     }
 }
