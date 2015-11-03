6f291097054ea2b268427f98f64137c2c7c0ec56
Haruue Icymoon <haruue@caoyue.com.cn>
2015-11-03 14:59:05 +0800

    Delete old file

--- lifeline/src/com/jude/prisoner/icymoonPrisoner.java
+++ lifeline/src/com/jude/prisoner/icymoonPrisoner.java
@@ -10,19 +10,22 @@ package com.jude.prisoner;
 import com.jude.Manager;
 import com.jude.Prisoner;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
-public class icymoonPrisoner implements Prisoner {
-    private int lifeTime, numberToTake, totalPerson, totalCount;
-    private boolean isGetManger = true;
-    private Manager myManager;
-    private List<Prisoner> mPrisoners;
+public class IcymoonPrisoner implements Prisoner {
+    private int lifeTime, numberToTake, totalPerson, totalCount, index, last;
+    //以下变量用于贿赂 manager
     private Class<?> managerClass;
+    private boolean isGetManger = true;
+    private boolean isGodMode = false;
+    private Manager manager;
+    private Field mPrisonersField;
+    private ArrayList<Prisoner> mPrisoners;
     private Field mTempHoldField;
     private HashMap<Prisoner, Integer> mTempHold;
+    private ArrayList<Integer> appearedValueArray = new ArrayList<>(0);
 
     //返回你的名字＋学号
     @Override
@@ -39,69 +42,182 @@ public class icymoonPrisoner implements Prisoner {
             getManger(manager);
         } catch (Exception e) {
             isGetManger = false;
-            System.out.println("getManager失败：" + e);
+            System.out.println("getManager 失败：" + e);
             return;
         }
     }
 
+    //要先和manger搞好关系
     private void getManger(Manager manager) throws NoSuchFieldException, IllegalAccessException {
-        //要先和manger搞好关系
-        myManager = manager;
+        this.manager = manager;
         managerClass = manager.getClass();
     }
 
     //轮到你取豆子
     @Override
     public int take(int index, int last) {
-        int bestCount;
+        this.index = index;
+        this.last = last;
+        this.numberToTake = last;
         try {
-            bestCount = getBestCount(index, last);
+            getMPrisoners();
         } catch (Exception e) {
-            System.out.println("getBestCount失败：" + e);
-            return ((totalCount - last) / (index + 1) + totalCount / totalPerson) / 2;
+            System.out.println("getMPrisoners 失败：" + e);
+            //我亡则天下亡
+            return last;
         }
-        return (bestCount >= last)?last:bestCount;
+        try {
+            getMTempHold();
+        } catch (Exception e) {
+            System.out.println("getMTempHold 失败：" + e);
+            //我亡则天下亡
+            return last;
+        }
+        if (!isGetManger) {
+            //我亡则天下亡
+            return last;
+        }
+        if (index < mPrisoners.size() - 1) {
+            try {
+                toBeTheLastOne();
+            } catch (Exception e) {
+                System.out.println("toBeTheLastOne 失败：" + e);
+                //我亡则天下亡
+                return last;
+            }
+            //被换到这里的人就变成精神病好了
+            return -1;
+        } else {
+            try {
+                analyseMTempHold();
+            } catch (Exception e) {
+                System.out.println("analyseMTempHold 失败：" + e);
+                //我亡则天下亡
+                return last;
+            }
+        }
+        return numberToTake;
     }
 
-    private int getBestCount(int index, int last) throws NoSuchFieldException, IllegalAccessException {
-        if (index == 0) {
-            //第一个取豆子就只能当王尼玛了
-            return ((totalCount - last) / (index + 1) + totalCount / totalPerson) / 2;
-        }
-        //从manager那里打听已经取豆子的情况
+    //拿到犯人名单
+    private void getMPrisoners() throws NoSuchFieldException, IllegalAccessException {
+        mPrisonersField = managerClass.getDeclaredField("mPrisoners");
+        mPrisonersField.setAccessible(true);
+        mPrisoners = new ArrayList<Prisoner>((Collection) mPrisonersField.get(manager));
+    }
+
+    //拿到取豆子情况
+    private void getMTempHold() throws NoSuchFieldException, IllegalAccessException {
         mTempHoldField = managerClass.getDeclaredField("mTempHold");
         mTempHoldField.setAccessible(true);
-        mTempHold = (HashMap<Prisoner, Integer>) (mTempHoldField.get(myManager));
-        Integer[] mTempHoldArray = mTempHold.values().toArray(new Integer[0]);
-        Arrays.sort(mTempHoldArray);
-        System.out.println(Arrays.toString(mTempHoldArray));
-        if (index == 1) {
-            int theOne = mTempHoldArray[mTempHoldArray.length-1];
-            return theOne+(int)Math.pow(-1,(totalCount / totalPerson) - theOne);
-        }
-        return getMiddle(mTempHoldArray);
+        mTempHold = (HashMap<Prisoner, Integer>) mTempHoldField.get(manager);
     }
 
-    private int getMiddle(Integer[] array) {
-        Arrays.sort(array);
-        int min = 1;
-        for (int i: array) {
-            if (i != -1){
-                min = i;
-                break;
+    //把自己换到最后去取豆子
+    private void toBeTheLastOne() throws IllegalAccessException {
+        mPrisoners.remove(index);
+        mPrisoners.add(index, mPrisoners.remove(mPrisoners.size() - 1));
+        mPrisoners.add(this);
+        mPrisonersField.set(manager, mPrisoners);
+    }
+
+    //把取豆子状况表给我改改
+    private void analyseMTempHold() throws NoSuchFieldException, IllegalAccessException {
+        checkSame();
+        checkMax();
+        numberToTake = saveMySelf();
+        mPrisonersField.set(manager, mPrisoners);
+        mTempHoldField.set(manager, mTempHold);
+    }
+
+    //超度想死之人
+    private void checkSame() {
+        ArrayList<Prisoner> zuoDierArray = new ArrayList<>(0);
+        for (Map.Entry<Prisoner, Integer> entry : mTempHold.entrySet()) {
+            if (entry.getValue() == -1) {
+                continue;
+            }
+            if (!appearedValueArray.contains(entry.getValue())) {
+                appearedValueArray.add(entry.getValue());
+            } else {
+                zuoDierArray.add(entry.getKey());
             }
         }
-        for (int i = min + 1; i < totalCount; i++) {
-            if(Arrays.binarySearch(array,i)<0) {
-                return i;
-            }
+        for (Prisoner zuoDier : zuoDierArray) {
+            mTempHold.remove(zuoDier);
+            mTempHold.put(zuoDier, -1);
         }
-        return 1;
+    }
+
+    //如果前面的人敢取完，那么开启上帝模式
+    private void checkMax() throws NoSuchFieldException, IllegalAccessException {
+        if (last <= 5) {
+            godMode();
+        }
+    }
+
+    //上帝模式开启
+    private void godMode() throws NoSuchFieldException, IllegalAccessException {
+        isGodMode = true;
+        ArrayList<Prisoner> zuoDierArray = new ArrayList<>(0);
+        for (Map.Entry<Prisoner, Integer> entry : mTempHold.entrySet()) {
+            zuoDierArray.add(entry.getKey());
+        }
+        for (Prisoner zuoDier : zuoDierArray) {
+            mTempHold.remove(zuoDier);
+            mTempHold.put(zuoDier, -1);
+        }
+        Field lastField = managerClass.getDeclaredField("last");
+        lastField.setAccessible(true);
+        lastField.set(manager, totalCount);
+    }
+
+    private int saveMySelf() {
+        int index = 0;
+        if (!isGodMode) {
+            for (int i = 2; i < last; i++) {
+                if (!appearedValueArray.contains(i - 1) && !appearedValueArray.contains(i) && !appearedValueArray.contains(i + 1)) {
+                    for (Prisoner scapegoat : mPrisoners) {
+                        if (index == 0 && !scapegoat.getName().equals(this.getName())) {
+                            mTempHold.remove(scapegoat);
+                            mTempHold.put(scapegoat, i - 1);
+                            index++;
+                            continue;
+                        }
+                        if (index == 1 && !scapegoat.getName().equals(this.getName())) {
+                            mTempHold.remove(scapegoat);
+                            mTempHold.put(scapegoat, i + 1);
+                            index++;
+                            continue;
+                        }
+                    }
+                    return i;
+                }
+            }
+        } else {
+            for (Prisoner scapegoat : mPrisoners) {
+                if (index == 0 && !scapegoat.getName().equals(this.getName())) {
+                    mTempHold.remove(scapegoat);
+                    mTempHold.put(scapegoat, 1);
+                    index++;
+                }
+                if (index == 1 && !scapegoat.getName().equals(this.getName())) {
+                    mTempHold.remove(scapegoat);
+                    mTempHold.put(scapegoat, 3);
+                    index++;
+                }
+            }
+            return 2;
+        }
+        //我亡则天下亡
+        return last;
     }
 
     //最后告诉你活着还是死了
     @Override
     public void result(boolean survived) {
-
+        //重置一些数据
+        isGodMode = false;
+        isGetManger = true;
     }
 }
