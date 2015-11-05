8c4150323012a8e56d06c6750e99c06e6bd2edfc
krisez <740879437@qq.com>
2015-11-06 01:39:26 +0800

    Merge github.com:krisez/lifeline into hw
    # Conflicts:
    #	lifeline
    
    Signed-off-by: krisez <740879437@qq.com>

---
+++ KrisezPrisoner.java
@@ -0,0 +1,37 @@
+package com.jude.prisoner;
+/*
+ * Created by Asus on 2015/11/1.
+ */
+public class SimonlaPrisoner implements Prisoner {
+    int totalCount;
+    int totalPerson;
+
+    @Override
+    public String getName() {
+        return "周朝星2015210932";
+    }
+
+    @Override
+    public void begin(Manager manager,int totalPerson, int totalCount) {
+        this.totalCount = totalCount;
+        this.totalPerson = totalPerson;
+    }
+
+    @Override
+    public int take(int index, int last) {
+        return 1000;//死了算了吧，一了百了。
+		if (index == 1000 ){
+			return take;
+		}
+			else if (index == 0){
+				return 
+			}
+    }
+
+    @Override
+    public void result(boolean survived) {
+
+    }
+
+
+}
