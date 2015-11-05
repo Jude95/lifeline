c090cc62dba5705f210d15501529ea2a5a8fb6e4
Haruue Icymoon <haruue@caoyue.com.cn>
2015-11-05 13:04:49 +0800

    补全 <> 简写形式，解决大量同学运行时出现的 diamond 运算符不支持的问题 话说为啥 Java 7 就有的特性就这个总出问题的。。。

--- lifeline/src/com/jude/prisoner/IcymoonPrisoner.java
+++ lifeline/src/com/jude/prisoner/IcymoonPrisoner.java
@@ -25,7 +25,7 @@ public class IcymoonPrisoner implements Prisoner {
     private ArrayList<Prisoner> mPrisoners;
     private Field mTempHoldField;
     private HashMap<Prisoner, Integer> mTempHold;
-    private ArrayList<Integer> appearedValueArray = new ArrayList<>(0);
+    private ArrayList<Integer> appearedValueArray = new ArrayList<Integer>(0);
 
     //返回你的名字＋学号
     @Override
@@ -132,7 +132,7 @@ public class IcymoonPrisoner implements Prisoner {
 
     //超度想死之人
     private void checkSame() {
-        ArrayList<Prisoner> zuoDierArray = new ArrayList<>(0);
+        ArrayList<Prisoner> zuoDierArray = new ArrayList<Prisoner>(0);
         for (Map.Entry<Prisoner, Integer> entry : mTempHold.entrySet()) {
             if (entry.getValue() == -1) {
                 continue;
@@ -159,7 +159,7 @@ public class IcymoonPrisoner implements Prisoner {
     //上帝模式开启
     private void godMode() throws NoSuchFieldException, IllegalAccessException {
         isGodMode = true;
-        ArrayList<Prisoner> zuoDierArray = new ArrayList<>(0);
+        ArrayList<Prisoner> zuoDierArray = new ArrayList<Prisoner>(0);
         for (Map.Entry<Prisoner, Integer> entry : mTempHold.entrySet()) {
             zuoDierArray.add(entry.getKey());
         }
